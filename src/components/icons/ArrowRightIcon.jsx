f3379afd7b288fa1b8a1a29eb083e3b01a0d09a2
mrz khrmn <mrzkhrmn@hotmail.com>
2026-02-03 01:59:07 +0300

    Hakkımızda kısmına kadar responsive tasarım yapıldı (hakkımızda ki video kısmı hariç)

--- src/components/icons/ArrowRightIcon.jsx
+++ src/components/icons/ArrowRightIcon.jsx
@@ -1,23 +1,31 @@
-
-
-const ArrowRightIcon = () => {
+const ArrowRightIcon = ({ color = "#F2F2F3" }) => {
   return (
-    <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0_60_820)">
-<g clip-path="url(#clip1_60_820)">
-<path d="M8.15446 0.42231C8.15446 1.13196 8.85773 2.19389 9.56866 3.08572C10.4841 4.23395 11.5768 5.23702 12.8304 6.00288C13.7696 6.57634 14.9101 7.12684 15.8263 7.12684M8.15446 13.8479C8.15446 13.1383 8.85773 12.0764 9.56866 11.1846C10.4841 10.0364 11.5768 9.03324 12.8304 8.26739C13.7696 7.69393 14.9101 7.14342 15.8263 7.14342M15.8263 7.13513H0.482666" stroke="#F2F2F3" stroke-width="1.01932"/>
-</g>
-</g>
-<defs>
-<clipPath id="clip0_60_820">
-<rect width="16.3091" height="16.3091" fill="white"/>
-</clipPath>
-<clipPath id="clip1_60_820">
-<rect width="16.3091" height="14.2705" fill="white"/>
-</clipPath>
-</defs>
-</svg>
-  )
-}
+    <svg
+      width="17"
+      height="17"
+      viewBox="0 0 17 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_60_820)">
+        <g clip-path="url(#clip1_60_820)">
+          <path
+            d="M8.15446 0.42231C8.15446 1.13196 8.85773 2.19389 9.56866 3.08572C10.4841 4.23395 11.5768 5.23702 12.8304 6.00288C13.7696 6.57634 14.9101 7.12684 15.8263 7.12684M8.15446 13.8479C8.15446 13.1383 8.85773 12.0764 9.56866 11.1846C10.4841 10.0364 11.5768 9.03324 12.8304 8.26739C13.7696 7.69393 14.9101 7.14342 15.8263 7.14342M15.8263 7.13513H0.482666"
+            stroke={color}
+            stroke-width="1.01932"
+          />
+        </g>
+      </g>
+      <defs>
+        <clipPath id="clip0_60_820">
+          <rect width="16.3091" height="16.3091" fill="white" />
+        </clipPath>
+        <clipPath id="clip1_60_820">
+          <rect width="16.3091" height="14.2705" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
 
-export default ArrowRightIcon
+export default ArrowRightIcon;
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,4 @@
 export { default as SearchIcon } from "./SearchIcon.jsx";
 export { default as TurkeyFlag } from "./TurkeyFlag.jsx";
 export { default as ArrowRightIcon } from "./ArrowRightIcon.jsx";
+export { default as CheckmarkIcon } from "./CheckmarkIcon.jsx";
