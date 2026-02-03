314022e2fc56fb5a3f56108165d2d387fa41b8dc
mrzkhrmn <mrzkhrmn@hotmail.com>
2026-02-03 17:44:26 +0300

    image fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mrzkhrmn.github.io',
